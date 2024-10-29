e8942e69e7eb74841506f9b8ab3348ca5165f35c
alessandroguddo02 <guddoalessandro6@gmail.com>
2024-10-29 14:39:29 +0100

    multipli di 3

--- script/script.js
+++ script/script.js
@@ -27,6 +27,10 @@ for (let i = 1; i <= tNumber; i++) {
         console.log("FizzBuzz")
     else if(i % 5 === 0)
         console.log("Buzz")
+    else if(i % 3 === 0)
+        console.log("Fizz")
+    
+
     
     
 
